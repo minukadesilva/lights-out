ec942249d7bb1ff042cbe899a8e921184a5b3ed9
Minuka De Silva <desilvaminuka@gmail.com>
2026-08-01 18:29:39 +0530

    Initial project setup-part 2

---
+++ assets/js/script.js
@@ -0,0 +1,1 @@
+console.log("Lights Out Loaded");
--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
 declare(strict_types=1);
+?>
 
-echo "<h1>Lights Out</h1>";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+
+    <title>Lights Out</title>
+
+    <link rel="stylesheet"
+          href="assets/css/style.css">
+
+</head>
+
+<body>
+
+    <h1>🏎️ Lights Out</h1>
+
+    <script src="assets/js/script.js"></script>
+
+</body>
+
+</html>
